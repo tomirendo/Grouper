9ee4ce7f81c11e731fbb03c5305dcbb721df218a
Yotam Vaknin <yotamvaknin@Yotams-MacBook-Pro.local>
2014-08-17 03:19:01 +0300

    Added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Grouper',
+      version='0.1',
+      description='A python library for grouping objects together',
+      author='Yotam Vaknin',
+      author_email='tomirendo@gmail.com',
+      url='https://github.com/tomirendo/Grouper',
+      packages=['Grouper'],
+     )
