800f0f9e70cd3c152e787380f21e5c1569777427
Yotam Vaknin <yotamvaknin@Yotams-MacBook-Pro.local>
2014-08-17 03:20:25 +0300

    Added Group library

---
+++ Grouper/Grouper.py
@@ -0,0 +1,64 @@
+class groupby(object):
+    def __init__(self, iterable, key=None,relation = None,compare_to_first = False,as_iterable = True):
+        self.it = iter(iterable)
+        self.as_iterable = as_iterable
+        if relation is None:
+            if key is None:
+                key = lambda x: x
+            self.keyfunc = key
+            self.relation_function = None
+            self.tgtkey = self.currkey = self.currvalue = object()
+        else :
+            self.relation_function = relation
+            self.prevvalue = None
+            self.currvalue = next(self.it)
+            self.it_ended = False
+            self.cmp_to_first = compare_to_first
+            
+    def __iter__(self):
+        return self
+    
+    def next(self):
+        if self.relation_function is None:
+            while self.currkey == self.tgtkey:
+                self.currvalue = next(self.it)    # Exit on StopIteration
+                self.currkey = self.keyfunc(self.currvalue)
+            self.tgtkey = self.currkey
+            if self.as_iterable:
+                return (self.currkey, self._grouper(self.tgtkey))
+            else :
+                return (self.currkey, list(self._grouper(self.tgtkey)))
+        else :
+            if not self.it_ended:
+                if self.as_iterable:
+                    return self._relation_grouper()
+                else :
+                    return list(self._relation_grouper())
+            else :
+                raise StopIteration()
+        
+    def _grouper(self, tgtkey):
+        while self.currkey == tgtkey:
+            yield self.currvalue
+            self.currvalue = next(self.it)    # Exit on StopIteration
+            self.currkey = self.keyfunc(self.currvalue)
+
+    def _relation_grouper(self):
+        yield self.currvalue
+        self.prevvalue = self.currvalue
+        try:
+            self.currvalue = next(self.it)
+        except StopIteration:
+            self.it_ended = True
+            raise StopIteration
+        while self.relation_function(self.prevvalue,self.currvalue):
+            yield self.currvalue
+            if self.cmp_to_first is False:
+                self.prevvalue = self.currvalue
+            try :
+                self.currvalue = next(self.it)
+            except StopIteration:
+                self.it_ended = True
+                raise StopIteration()
+        
+        
